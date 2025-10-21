9c844b599b4702390f800f1262cbf8101d33ac3a
Zhengli Sunzhu <1798600@uab.cat>
2025-10-21 12:16:45 +0200

    Update programa_principal.py

--- programa_principal.py
+++ programa_principal.py
@@ -1,5 +1,32 @@
 PresentacioJoc()
 import random
 player1 = int(input("Introdueix el teu nom: "))
-player2 = int(input("Introdueix el teu nom: "))
 random.seed(<player1>)
+num = int(input("Introdueix el nombre de partides: ")
+total_partides == LlegirSenar()
+guanys_necessaris = total_partides //2 + 1
+Sheldon_guanya = 0
+player1_guanya = 0
+Sheldon = []
+player1 = []
+
+while (Sheldon_guanya < guanys_necessaris) or (player1_guanya < guanys_necessaris):
+  Sheldon_eleccio = random.randint(1,5)
+  MenuRPSLS()
+  player1 == LlegirNombre(randint(1,5))
+  resultat == JocRPSLS(player1, Sheldon)
+  MissatgeRPLSLS(player1, Sheldon)
+  if resultat == 2:
+    Sheldon_guanya +=1
+    print("Guanya Sheldon Cooper!!!")
+  elif resultat == 1:
+    player1_guanya += 1
+    print(f"Guanya {player1}")
+  else:
+    pass
+  
+
+
+print("Marcador -- Sheldon {Sheldon_guanya} {player1} {player1_guanya}")
+if Sheldon_guanya 
+  
