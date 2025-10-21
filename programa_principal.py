029d698c96a13a46b925245eb74a8d4f912008dc
Leo <1707505@uab.cat>
2025-10-21 12:17:49 +0200

    Refactor game logic and improve variable names

--- programa_principal.py
+++ programa_principal.py
@@ -1,32 +1,46 @@
-PresentacioJoc()
-import random
-player1 = int(input("Introdueix el teu nom: "))
-random.seed(<player1>)
-num = int(input("Introdueix el nombre de partides: ")
-total_partides == LlegirSenar()
-guanys_necessaris = total_partides //2 + 1
-Sheldon_guanya = 0
-player1_guanya = 0
-Sheldon = []
-player1 = []
+import random 
 
-while (Sheldon_guanya < guanys_necessaris) or (player1_guanya < guanys_necessaris):
-  Sheldon_eleccio = random.randint(1,5)
-  MenuRPSLS()
-  player1 == LlegirNombre(randint(1,5))
-  resultat == JocRPSLS(player1, Sheldon)
-  MissatgeRPLSLS(player1, Sheldon)
-  if resultat == 2:
-    Sheldon_guanya +=1
-    print("Guanya Sheldon Cooper!!!")
-  elif resultat == 1:
-    player1_guanya += 1
-    print(f"Guanya {player1}")
-  else:
-    pass
-  
+if __name__ == "__main__":
+    PresentacioJoc()
 
+    nom_jugador = input("Introdueix el teu nom: ")
+    random.seed(nom_jugador)
 
-print("Marcador -- Sheldon {Sheldon_guanya} {player1} {player1_guanya}")
-if Sheldon_guanya 
-  
+    total_partides = LlegirSenar()
+
+    guanys_necessaris = total_partides // 2 + 1
+
+    guanys_sheldon = 0
+    guanys_jugador = 0
+    llista_sheldon = []
+    llista_jugador = []
+
+    while guanys_sheldon < guanys_necessaris and guanys_jugador < guanys_necessaris:
+        sheldon_eleccio = random.randint(1, 5)
+    
+    MenuRPSLS()
+
+    eleccio_jugador = LlegirNombre(1, 5)
+
+    resultat = JocRPSLS(sheldon_eleccio, eleccio_jugador)
+
+    MissatgeRPSLS(sheldon_eleccio, eleccio_jugador)
+    if resultat == 1:
+        print("Guanya Sheldon Cooper!!!")
+    elif resultat == 2:
+        print(f"Guanya {nom_jugador}!!!")
+    else:
+        pass
+
+    print(f"Marcador -- Sheldon: {guanys_sheldon} | {nom_jugador}: {guanys_jugador}\n")
+
+    if guanys_sheldon > guanys_jugador:
+        print("El guanyador és Sheldon")
+    else:
+        print(f"El guanyador és {nom_jugador}")
+    
+    sheldon_noms = [_figura_nom(x) for x in llista_sheldon]
+    jugador_noms = [_figura_nom(x) for x in llista_jugador]
+    print("Figures Sheldon:", sheldon_noms)
+    print(f"Figures {nom_jugador}:", jugador_noms)
+    
