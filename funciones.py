fabc04621ca1ed97cb2452e4cb8b70624593f1eb
Zhengli Sunzhu <1798600@uab.cat>
2025-10-21 11:38:23 +0200

    Update funciones.py

--- funciones.py
+++ funciones.py
@@ -3,6 +3,51 @@ PAPER = 2
 SCISSORS = 3
 LIZARD = 4
 SPOCK = 5
+def PresentacioJoc():
+    print("Pedra, paper, tisores, llangardaix, Spock és un joc d'atzar ampliació del popular Pedra, paper, tisores. Creat per Sam Kass amb Karen Bryla http://www.samkass.com/theories/RPSSL.html. " \
+    "Popularitzat per Sheldon Cooper a la sèrie Big Bang Theory. " \
+    "Es fa servir per solucionar una disputa entre Sheldon i Raj en el capítol The Lizard - Spock Expansion. " \
+    "" \
+    "El joc és al millor de N partides on N és un nombre senar ")
+
+def Senar(num):
+    if num %2 != 0:
+        senar = True
+    else:
+        senar = False
+    return senar
+
+def LlegirSenar():
+    num = int(input("Introduir un nombre senar: "))
+    Senar(num)
+    if senar == True:
+        return num
+    else:
+        while senar != True:
+            print("ERROR: El nombre introduït és parell")
+            num = int(input("Introduir un nombre senar: "))
+            Senar(num)
+            if senar == True:
+                return num
+
+def MenuRPSLS():
+    print("Selecciona una opció:")
+    print("1. Pedra")
+    print("2. Paper")
+    print("3. Tisores")
+    print("4. Llangardaix")
+    print("5. Spock")
+    
+def LlegirNombre(minim,maxim):
+    maxim = 5
+    minim = 1
+
+    if minim < maxim:
+        print(f"Entra valor entre {maxim} i {minim}")
+        return [minim,maxim]
+    else:
+        print("ERROR: Valor fora de l'interval ")
+        LlegirNombre(minim,maxim)
 
 def MissatgeRPSLS(player1, player2):
     if player1 == player2:
